78087411143078d203b0415b70752bf24b4b524d
stelatoris <porchpu@gmail.com>
2024-02-29 22:28:38 +0100

    Added ejs-mate boilerplate style. Edit, Delete routes.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const campground = require('./models/campground');
 
@@ -15,11 +17,12 @@ db.once("open", () => {
 
 const app = express();
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 const port = 3000;
 
@@ -41,6 +44,24 @@ app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', { campground });
 })
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
 
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
